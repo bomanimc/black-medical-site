4e8a2e39076a7820c28622a123ea46a0e1224c9d
Bomani McClendon <bomanimcclendon2017@u.northwestern.edu>
2019-05-04 12:44:33 -0400

    Add basic page titles

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,38 @@ import React from "react";
 import styled from "styled-components";
 import { graphql } from "gatsby";
 
+const Root = styled.div`
+  font-family: 'Lora', 'Times New Roman';
+  color: white;
+  margin: 32px 64px;
+`;
+
+const SiteTitle = styled.h1`
+  font-size: 72px;
+  font-weight: bold;
+`;
+
+const Subtitle = styled.p`
+  font-size: 24px;
+  margin: 16px 0px;
+
+  a {
+    color: white;
+  }
+
+  a:visited {
+    color: white;
+  }
+`;
+
 const IndexPage = ({ data }) => {
   console.log(data);
 
   return (
-    <div />
+    <Root>
+      <SiteTitle>On Black People & Medicine</SiteTitle>
+      <Subtitle>An ongoing research project by <a href="http://bomani.xyz/">Bomani Oseni McClendon</a>. </Subtitle>
+    </Root>
   );
 };
 
